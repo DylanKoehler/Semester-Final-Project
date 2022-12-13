7773b82cba2331ead9b1c4efdbb4127695c8a1a2
DylanKoehler <dylankoehler3212@gmail.com>
2022-12-13 09:29:26 -0600

    Initial Commit

---
+++ Semester Final Project/Semester_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Semester_Final_ProjectApp.swift
+//  Semester Final Project
+//
+//  Created by Dylan Koehler on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Semester_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
